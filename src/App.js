4e79d9b8991a19c56273139bb61d5438c47bbe9f
Plamen Uzunov <plamen.uzunov@strypes.eu>
2023-01-23 16:01:46 +0200

    app finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Box } from '@mui/material'
-import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
+
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
 const App = () => (
   <BrowserRouter>
